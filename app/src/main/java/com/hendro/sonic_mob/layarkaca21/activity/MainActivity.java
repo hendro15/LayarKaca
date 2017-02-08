ee755047db0501ff1c3f0e8354418e547da810f5
Maulina Nur Istiqomah <maulina12@mhs.if.its.ac.id>
2017-02-08 14:37:10 +0700

    code MainActivity blm selesai

--- app/src/main/java/com/hendro/sonic_mob/layarkaca21/activity/MainActivity.java
+++ app/src/main/java/com/hendro/sonic_mob/layarkaca21/activity/MainActivity.java
@@ -1,31 +1,141 @@
 package com.hendro.sonic_mob.layarkaca21.activity;
 
-import android.app.Activity;
 import android.os.Bundle;
+import android.os.Handler;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
 import com.hendro.sonic_mob.layarkaca21.R;
+import com.hendro.sonic_mob.layarkaca21.other.CircleTransform;
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
+
+    private NavigationView navigationView;
+    private DrawerLayout drawer;
+    private View navHeader;
+    private ImageView imgNavHeaderBg, imgProfile;
+    private TextView txtName, txtWebsite;
+    private Toolbar toolbar;
+    private FloatingActionButton fab;
+
+    private static final String urlNavHeaderBg = "http://api.androidhive.info/images/nav-menu-header-bg.jpg";
+    private static final String urlProfileImg = "https://lh3.googleusercontent.com/eCtE_G34M9ygdkmOpYvCag1vBARCmZwnVS6rS5t4JLzJ6QgQSBquM0nuTsCpLhYbKljoyS-txg";
+
+    public static int navItemIndex = 0;
+
+    private static final String TAG_HOME = "home";
+    private static final String TAG_PHOTOS = "photos";
+    private static final String TAG_MOVIES = "movies";
+    private static final String TAG_NOTIF = "notifications";
+    private static final String TAG_SETTINGS = "settings";
+    private static String CURRENT_TAG = TAG_HOME;
+
+    private String[] activityTitles;
+
+    private boolean shouldLoadHomeFragOnBackPress = true;
+    private Handler mHandler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main_drawer);
+        setContentView(R.layout.activity_main);
 
-    // Example of a call to a native method
-    TextView tv = (TextView) findViewById(R.id.sample_text);
-    tv.setText(stringFromJNI());
+        toolbar = (Toolbar)findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        mHandler = new Handler();
+
+        drawer = (DrawerLayout)findViewById(R.id.drawer_layout);
+        navigationView = (NavigationView)findViewById(R.id.nav_view);
+        fab = (FloatingActionButton)findViewById(R.id.fab);
+
+        //navigation view header
+        navHeader = navigationView.getHeaderView(0);
+        txtName = (TextView)navHeader.findViewById(R.id.name);
+        txtWebsite = (TextView) navHeader.findViewById(R.id.website);
+        imgNavHeaderBg = (ImageView) navHeader.findViewById(R.id.img_header_bg);
+        imgProfile = (ImageView) navHeader.findViewById(R.id.img_profile);
+
+        activityTitles = getResources().getStringArray(R.array.nav_item_activity_title);
+
+        fab.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Actoion", null).show();
+            }
+        });
+
+        //load nav menu header data
+
+
     }
 
-    /**
-     * A native method that is implemented by the 'native-lib' native library,
-     * which is packaged with this application.
-     */
-    public native String stringFromJNI();
+    private void loadNavHeader(){
+        txtName.setText("Ravi Tamada");
+        txtWebsite.setText("www.androidhive.info");
 
-    // Used to load the 'native-lib' library on application startup.
-    static {
-        System.loadLibrary("native-lib");
+        // loading header background image
+        Glide.with(this).load(urlNavHeaderBg)
+                .crossFade()
+                .diskCacheStrategy(DiskCacheStrategy.ALL)
+                .into(imgNavHeaderBg);
+
+        // Loading profile image
+        Glide.with(this).load(urlProfileImg)
+                .crossFade()
+                .thumbnail(0.5f)
+                .bitmapTransform(new CircleTransform(this))
+                .diskCacheStrategy(DiskCacheStrategy.ALL)
+                .into(imgProfile);
+
+        navigationView.getMenu().getItem(3).setActionView(R.layout.menu_dot);
     }
+
+    private void loadHomeFragment(){
+        // selecting appropriate nav menu item
+        selectNavMenu();
+
+        // set toolbar title
+        setToolbarTitle();
+
+        // if user select the current navigation menu again, don't do anything
+        // just close the navigation drawer
+        if (getSupportFragmentManager().findFragmentByTag(CURRENT_TAG) != null){
+            drawer.closeDrawers();
+            toggleFab();
+            return;
+        }
+
+
+
+    }
+
+    private void setToolbarTitle(){
+        getSupportActionBar().setTitle(activityTitles[navItemIndex]);
+    }
+
+    private void selectNavMenu(){
+        navigationView.getMenu().getItem(navItemIndex).setChecked(true);
+    }
+
+    // show or hide the fab
+    private void toggleFab(){
+        if(navItemIndex == 0){
+            fab.show();
+        }else {
+            fab.hide();
+        }
+    }
+
 }
