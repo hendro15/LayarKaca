7b812a35afa2b70dac37db464fe8c0ee01297683
Maulina Nur Istiqomah <maulina12@mhs.if.its.ac.id>
2017-02-09 13:44:37 +0700

    All done, left for cheched

--- app/src/main/java/com/hendro/sonic_mob/layarkaca21/activity/MainActivity.java
+++ app/src/main/java/com/hendro/sonic_mob/layarkaca21/activity/MainActivity.java
@@ -1,20 +1,33 @@
 package com.hendro.sonic_mob.layarkaca21.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.engine.DiskCacheStrategy;
 import com.hendro.sonic_mob.layarkaca21.R;
+import com.hendro.sonic_mob.layarkaca21.fragment.HomeFragment;
+import com.hendro.sonic_mob.layarkaca21.fragment.MoviesFragment;
+import com.hendro.sonic_mob.layarkaca21.fragment.NotificationsFragment;
+import com.hendro.sonic_mob.layarkaca21.fragment.PhotosFragment;
+import com.hendro.sonic_mob.layarkaca21.fragment.SettingsFragment;
 import com.hendro.sonic_mob.layarkaca21.other.CircleTransform;
 
 public class MainActivity extends AppCompatActivity {
@@ -49,25 +62,25 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        toolbar = (Toolbar)findViewById(R.id.toolbar);
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
         mHandler = new Handler();
 
-        drawer = (DrawerLayout)findViewById(R.id.drawer_layout);
-        navigationView = (NavigationView)findViewById(R.id.nav_view);
-        fab = (FloatingActionButton)findViewById(R.id.fab);
+        drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        navigationView = (NavigationView) findViewById(R.id.nav_view);
+        fab = (FloatingActionButton) findViewById(R.id.fab);
 
         //navigation view header
         navHeader = navigationView.getHeaderView(0);
-        txtName = (TextView)navHeader.findViewById(R.id.name);
+        txtName = (TextView) navHeader.findViewById(R.id.name);
         txtWebsite = (TextView) navHeader.findViewById(R.id.website);
         imgNavHeaderBg = (ImageView) navHeader.findViewById(R.id.img_header_bg);
         imgProfile = (ImageView) navHeader.findViewById(R.id.img_profile);
 
         activityTitles = getResources().getStringArray(R.array.nav_item_activity_title);
 
-        fab.setOnClickListener(new View.OnClickListener(){
+        fab.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View view) {
@@ -77,11 +90,19 @@ public class MainActivity extends AppCompatActivity {
         });
 
         //load nav menu header data
+        loadNavHeader();
 
+        // initializing navigation menu
+        setUpNavigationView();
 
+        if (savedInstanceState == null) {
+            navItemIndex = 0;
+            CURRENT_TAG = TAG_HOME;
+            loadHomeFragment();
+        }
     }
 
-    private void loadNavHeader(){
+    private void loadNavHeader() {
         txtName.setText("Ravi Tamada");
         txtWebsite.setText("www.androidhive.info");
 
@@ -102,7 +123,7 @@ public class MainActivity extends AppCompatActivity {
         navigationView.getMenu().getItem(3).setActionView(R.layout.menu_dot);
     }
 
-    private void loadHomeFragment(){
+    private void loadHomeFragment() {
         // selecting appropriate nav menu item
         selectNavMenu();
 
@@ -111,31 +132,232 @@ public class MainActivity extends AppCompatActivity {
 
         // if user select the current navigation menu again, don't do anything
         // just close the navigation drawer
-        if (getSupportFragmentManager().findFragmentByTag(CURRENT_TAG) != null){
+        if (getSupportFragmentManager().findFragmentByTag(CURRENT_TAG) != null) {
             drawer.closeDrawers();
+
+            // show or hide the fab button
             toggleFab();
             return;
         }
 
+        // Sometimes, when fragment has huge data, screens seems hanging
+        // when switching between navigation menus
+        // So using runnable, the fragment is loaded with cross fade effect
+        // This effect can be seen in GMail app
+        Runnable mPendingRunnable = new Runnable() {
+            @Override
+            public void run() {
+                // update the main content by replacing fragments
+                Fragment fragment = getHomeFragment();
+                FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+                fragmentTransaction.setCustomAnimations(android.R.anim.fade_in, android.R.anim.fade_out);
+                fragmentTransaction.replace(R.id.frame, fragment, CURRENT_TAG);
+                fragmentTransaction.commitAllowingStateLoss();
+            }
+        };
 
+        // If mPendingRunnable is not full, then add the message queue
+        if (mPendingRunnable != null) {
+            mHandler.post(mPendingRunnable);
+        }
+
+        // show or hide the fab button
+        toggleFab();
+
+        // closing drawer on item click
+        drawer.closeDrawers();
+
+        // refresh toolbar menu
+        invalidateOptionsMenu();
 
     }
 
-    private void setToolbarTitle(){
-        getSupportActionBar().setTitle(activityTitles[navItemIndex]);
-    }
-
-    private void selectNavMenu(){
-        navigationView.getMenu().getItem(navItemIndex).setChecked(true);
-    }
-
-    // show or hide the fab
-    private void toggleFab(){
-        if(navItemIndex == 0){
-            fab.show();
-        }else {
-            fab.hide();
+    private Fragment getHomeFragment() {
+        switch (navItemIndex) {
+            case 0:
+                HomeFragment homeFragment = new HomeFragment();
+                return homeFragment;
+            case 1:
+                PhotosFragment photosFragment = new PhotosFragment();
+                return photosFragment;
+            case 2:
+                MoviesFragment moviesFragment = new MoviesFragment();
+                return moviesFragment;
+            case 3:
+                NotificationsFragment notificationsFragment = new NotificationsFragment();
+                return notificationsFragment;
+            case 4:
+                SettingsFragment settingsFragment = new SettingsFragment();
+                return settingsFragment;
+            default:
+                return new HomeFragment();
         }
     }
 
+    private void setToolbarTitle() {
+        getSupportActionBar().setTitle(activityTitles[navItemIndex]);
+    }
+
+    private void selectNavMenu() {
+        navigationView.getMenu().getItem(navItemIndex).setChecked(true);
+    }
+
+    private void setUpNavigationView() {
+        //Setting Navigation View Item Selected Listener to handle the item click of the navigation menu
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+                                                             // This method will trigger on item Click of navigation menu
+                                                             @Override
+                                                             public boolean onNavigationItemSelected(MenuItem menuItem) {
+                                                                 // Check to see which item was being clicked and perform appropriate action
+                                                                 switch (menuItem.getItemId()) {
+                                                                     //Replacing the main content with ContentFragment which is our Inbox view
+                                                                     case R.id.nav_home:
+                                                                         navItemIndex = 0;
+                                                                         CURRENT_TAG = TAG_HOME;
+                                                                         break;
+                                                                     case R.id.nav_photos:
+                                                                         navItemIndex = 1;
+                                                                         CURRENT_TAG = TAG_PHOTOS;
+                                                                         break;
+                                                                     case R.id.nav_movies:
+                                                                         navItemIndex = 2;
+                                                                         CURRENT_TAG = TAG_MOVIES;
+                                                                         break;
+                                                                     case R.id.nav_notif:
+                                                                         navItemIndex = 3;
+                                                                         CURRENT_TAG = TAG_NOTIF;
+                                                                         break;
+                                                                     case R.id.nav_settings:
+                                                                         navItemIndex = 4;
+                                                                         CURRENT_TAG = TAG_SETTINGS;
+                                                                         break;
+                                                                     case R.id.nav_about_us:
+                                                                         // launch item intent instead of loading fragment
+                                                                         startActivity(new Intent(MainActivity.this, AboutUsActivity.class));
+                                                                         drawer.closeDrawers();
+                                                                         return true;
+                                                                     case R.id.nav_privacy_policy:
+                                                                         // launch new intent instead of laoding fragment
+                                                                         startActivity(new Intent(MainActivity.this, PrivacyPolicyActivity.class));
+                                                                         drawer.closeDrawers();
+                                                                         return true;
+                                                                     default:
+                                                                         navItemIndex = 0;
+                                                                 }
+
+                                                                 // Checking if the item is in checked state or not, if not make it in checked state
+                                                                 if (menuItem.isChecked()) {
+                                                                     menuItem.setChecked(false);
+                                                                 } else {
+                                                                     menuItem.setChecked(true);
+                                                                 }
+
+                                                                 menuItem.setChecked(true);
+
+                                                                 loadHomeFragment();
+
+                                                                 return true;
+                                                             }
+                                                         }
+        );
+
+        ActionBarDrawerToggle actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.openDrawer, R.string.closeDrawer) {
+
+            @Override
+            public void onDrawerClosed(View drawerView) {
+                // Code here will be trigged once the drawer closes as we dont anything happen so we leave this blank
+                super.onDrawerClosed(drawerView);
+            }
+
+            @Override
+            public void onDrawerOpened(View drawerView) {
+                // Code here will be trigged once the drawer closes as we dont anything happen so we leave this blank
+                super.onDrawerOpened(drawerView);
+            }
+        };
+
+        // Setting the actionbarToggle to drawer layout
+        drawer.setDrawerListener(actionBarDrawerToggle);
+
+        //calling sync state is necessary or else your hamburger icon wont show up
+        actionBarDrawerToggle.syncState();
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawers();
+            return;
+        }
+
+        // This code loads home fragment when back key is pressed'
+        // when user is in other fragment than home
+        if (shouldLoadHomeFragOnBackPress) {
+            // Checking if user is on other navigation menu
+            // rather than home
+            if (navItemIndex != 0) {
+                navItemIndex = 0;
+                CURRENT_TAG = TAG_HOME;
+                loadHomeFragment();
+                return;
+            }
+        }
+
+        super.onBackPressed();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present
+
+        // show menu only when home fragment is selected
+        if (navItemIndex == 0) {
+            getMenuInflater().inflate(R.menu.main, menu);
+        }
+
+        // when fragment is notifications, load the menu created for notifications
+        if (navItemIndex == 3) {
+            getMenuInflater().inflate(R.menu.notification, menu);
+        }
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        //Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml
+        int id = item.getItemId();
+
+        // no inspection SimplifiableStatement
+        if (id == R.id.action_logout) {
+            Toast.makeText(getApplicationContext(), "Logout user!", Toast.LENGTH_LONG).show();
+            return true;
+        }
+
+        // user is in notifications fragment
+        // and selected 'Mark all as Read'
+        if (id == R.id.action_mark_all_read) {
+            Toast.makeText(getApplicationContext(), "All notifications marked as read!", Toast.LENGTH_LONG).show();
+        }
+
+        // user is in notifications fragment
+        // and selected "Clear all"
+        if (id == R.id.action_clear_notification) {
+            Toast.makeText(getApplicationContext(), "Clear all notifications!", Toast.LENGTH_LONG).show();
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    // show or hide the fab
+    private void toggleFab() {
+        if (navItemIndex == 0) {
+            fab.show();
+        } else {
+            fab.hide();
+        }
+    }
 }
---
+++ app/src/main/java/com/hendro/sonic_mob/layarkaca21/activity/PrivacyPolicyActivity.java
@@ -0,0 +1,15 @@
+package com.hendro.sonic_mob.layarkaca21.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.hendro.sonic_mob.layarkaca21.R;
+
+public class PrivacyPolicyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_privacy_policy);
+    }
+}
